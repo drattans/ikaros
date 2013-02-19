bee6fdddb3cc5c2cf08391c763fe139c3e2da863
drattans <drattans@gmail.com>
2013-02-19 16:50:24 +0100

    Introducing a module with option of smooth movement.

---
+++ Source/UserModules/SmMo/SmMo.h
@@ -0,0 +1,31 @@
+#ifndef SmMo_
+#define SmMo_
+
+#include "IKAROS.h"
+
+class SmMo: public Module
+{
+ public:
+  static Module * Create(Parameter * p) { return new SmMo(p); }
+
+  float * cart_in;
+  float * servo_out;
+  float y1;
+  float y2;
+  float k;
+  float pi;
+  float fe;
+  float h;
+  float abcx;
+  float abcy;
+  bool smooth;  
+
+  SmMo(Parameter * p) : Module(p) {}
+  virtual ~SmMo() {}
+
+  void 		Init();
+  void 		Tick();
+};
+
+#endif
+
