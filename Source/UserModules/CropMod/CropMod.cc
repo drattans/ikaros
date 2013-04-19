0e2c9021814ad8ef03753e4053b8395cf0e81b34
drattans <drattans@gmail.com>
2013-04-19 13:00:41 +0200

    Modifications to CropMod.

--- Source/UserModules/CropMod/CropMod.cc
+++ Source/UserModules/CropMod/CropMod.cc
@@ -11,9 +11,6 @@ CropMod::Init()
   NoC = GetIntValue("NoC", 1);//Maximum number of clusters
   centerIn = GetInputMatrix("CENTER_IN");//Coordinates of found clusters (x,y)
   imout = GetOutputMatrix("IMOUT");//Croped image with block
-  //centerOut = GetOutputMatrix("CENTER_OUT");//Coordinates of modified clusters (x,y)
-  //radious = GetOutputArray("RADIOUS");//The largest radious of the object
-  //shor = GetOutputArray("SHOR");//Orientation of shape
 
   cropsif = 0.05f;//Half the length of the side of the croped image (fraction of large image)
   cropsip = 50;//sizeX*cropsif;//Half the length of the side of the croped image (pixels)
@@ -22,16 +19,43 @@ CropMod::Init()
 void
 CropMod::Tick()
 {
+  //centerIn[0][0]=centerIn[0][0]*640.f;
+  //centerIn[1][0]=centerIn[1][0]*480.f;
+  printf("X=%f, Y=%f\n", centerIn[0], centerIn[1]);
   NoC=1;//At least start with only one shape
   int i=1;
   //for(int i=0; i<NoC; ++i){//For all clusters
   //Crop out the object;
-  if(centerIn[i][0]-cropsip > 0 && centerIn[i][1]-cropsip > 0 && centerIn[i][0]+cropsip < sizeX && centerIn[i][1]+cropsip < sizeY){
+  if(centerIn[i][0]-cropsip >= 0 && centerIn[i][1]-cropsip >= 0 && centerIn[i][0]+cropsip < sizeX && centerIn[i][1]+cropsip < sizeY){
     for(int j=centerIn[i][0]-cropsip; j<centerIn[i][0]+cropsip; ++j){
       for(int k=centerIn[i][1]-cropsip; k<centerIn[i][1]+cropsip; ++k){
 	//Place pixel (j,k) in output matrix (i)
 	int a = j-(centerIn[i][0]-cropsip);
 	int b = k-(centerIn[i][1]-cropsip);
+	imout[a][b]=input[j][k];
+      }
+    }
+  }
+  else{//if it's close to the edge
+    int sx=centerIn[i][0]-cropsip;
+    int sy=centerIn[i][1]-cropsip;
+    if(centerIn[i][0]-cropsip < 0){
+      sx=0;
+    }
+    else if(centerIn[i][0]+cropsip >= sizeX){
+      sx=sizeX-2*cropsip;
+    }
+    if(centerIn[i][1]-cropsip < 0){
+      sy=0;
+    }
+    else if(centerIn[i][1]+cropsip >= sizeY){
+      sy=sizeY-2*cropsip;
+    }
+    for(int j=sx; j<sx+2*cropsip; ++j){
+      for(int k=sy; k<sy+2*cropsip; ++k){
+	//Place pixel (j,k) in output matrix (i)
+	int a = j-sx;
+	int b = k-sy;
 	imout[a][b]=input[j][k];
       }
     }
