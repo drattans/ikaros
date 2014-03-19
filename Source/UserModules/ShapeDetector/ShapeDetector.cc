2fa3abe42242bc50135c6591422774a8c4834bec
drattans <drattans@gmail.com>
2014-03-19 17:54:21 +0100

    Found the minus! :D

--- Source/UserModules/ShapeDetector/ShapeDetector.cc
+++ Source/UserModules/ShapeDetector/ShapeDetector.cc
@@ -143,10 +143,10 @@ void
 ShapeDetector::RecogniseShape()
 {
   int maximumAgreement=0;
-  int ko=0;
-  int jo=0;
-  int lo=0;
   for(int i=0; i<referenceShapes.size(); ++i){//For each shape (using brute force)
+    int ko;
+    int jo;
+    int lo=0;
     for(int l=0; l<numberOfRotations; ++l){//For each orientation
       for(int j=-10-boardSize; j<10-boardSize; ++j){//For each x-position:
 	for(int k=-10-boardSize; k<10-boardSize; ++k){//For each y-position:
@@ -154,6 +154,7 @@ ShapeDetector::RecogniseShape()
 	  agreement=Evaluate(j, k, l, i);//Check similarity
 	  if(agreement>maximumAgreement){//If it's the best agreement so far
 	    maximumAgreement=agreement;
+	    //printf("\n\nAm: %i\n\n\n",agreement);
 	    imageMetaData[0]=i;//Shape index
 	    imageMetaData[1]=l;//Orientation index
 	    imageMetaData[2]=inputCenters[0][0]+(k+boardSize);//New center in x
@@ -165,25 +166,25 @@ ShapeDetector::RecogniseShape()
 	    lo=l;
 	    //printf("IcSx: %f, IcSy: %f\n", inputCenters[0][0], inputCenters[0][1]);
 	    //printf("IcSx: %d, IcSy: %d\n", (k+boardSize), (j+boardSize));
-	    printf("3: IcSx: %f, IcSy: %f\n", imageMetaData[2], imageMetaData[3]);
+	    //printf("3: IcSx: %f, IcSy: %f\n", imageMetaData[2], imageMetaData[3]);
 	  }
 	}
       }
     }
+    int l=lo;
     //imageMetaData[0]=0;//Shape index
     //imageMetaData[1]=0;//Orientation index
-    imageMetaData[2]=inputCenters[0][0]+(ko+boardSize);//New center in x
-    imageMetaData[3]=inputCenters[0][1]+(jo+boardSize);//New center in y
+    //imageMetaData[2]=inputCenters[0][0]+(ko+boardSize);//New center in x
+    //imageMetaData[3]=inputCenters[0][1]+(jo+boardSize);//New center in y
     //imageMetaData[4]=numberOfRotations;//referenceShapes[i].size();
     //imageMetaData[5]=50;//Largest radius;
-    printf("4:IcSx: %f, IcSy: %f\n", imageMetaData[2], imageMetaData[3]);
+    //printf("4:IcSx: %f, IcSy: %f\n", imageMetaData[2], imageMetaData[3]);
     //To be able to inspect how good the estimation is, the input and the guess is
     //merged.
-    //int l=lo;
-    for(int m=0; m<referenceShapes[i][lo].size(); ++m){//sizeX+2*boardSize
-      for(int n=0; n<referenceShapes[i][lo].size(); ++n){//sizeY+2*boardSize
+    for(int m=0; m<referenceShapes[i][l].size(); ++m){//sizeX+2*boardSize
+      for(int n=0; n<referenceShapes[i][l].size(); ++n){//sizeY+2*boardSize
 	if(jo+m>=0 && sizeX>jo+m && ko+n>=0 && sizeY>ko+n){//>=0
-	  outputImage[m][n]=inputImage[jo+m][ko+n]/10+referenceShapes[i][lo][m][n]*9/10;
+	  outputImage[m][n]=(inputImage[jo+m][ko+n]+referenceShapes[i][lo][m][n])/2;
 	}
       }
     }
@@ -196,8 +197,8 @@ int
 ShapeDetector::Evaluate(int jIn, int kIn, int lIn, int iIn)
 {
   int agreementTemporary=0;
-  for(int m=0; m<sizeX+2*boardSize; ++m){
-    for(int n=0; n<sizeY+2*boardSize; ++n){
+  for(int m=0; m<referenceShapes[iIn][lIn].size(); ++m){//sizeX+2*boardSize
+    for(int n=0; n<referenceShapes[iIn][lIn].size(); ++n){//sizeY+2*boardSize
       if(jIn+m>=0 && sizeX>jIn+m && kIn+n>=0 && sizeY>kIn+n){//>=0
 	//printf("C1a: x: %i, y: %i\n", jIn+m, kIn+n);
         if(inputImage[jIn+m][kIn+n]>0.5 && referenceShapes[iIn][lIn][m][n]>0.5){
@@ -205,9 +206,9 @@ ShapeDetector::Evaluate(int jIn, int kIn, int lIn, int iIn)
 	  ++agreementTemporary;
         }
       }
-      else{
+      //else{
 	//printf("C2: x: %i, y: %i\n", jIn+m, kIn+n);
-      }
+      //}
     }
   }
   return agreementTemporary;
@@ -253,6 +254,7 @@ ShapeDetector::LearnNewShape()
       } 
     }
   }
+  //printf("\nx1: %i, x2: %i, y1: %i, y2: %i\n\n", cropEdge[0], cropEdge[1], cropEdge[2], cropEdge[3]);
   //Add the cropped part to the center of a vectorvector
   int shapeLengthX = cropEdge[1]-cropEdge[0];//Largest distance of shape in x
   int shapeLengthY = cropEdge[3]-cropEdge[2];//Largest distance of shape in y
@@ -293,8 +295,8 @@ ShapeDetector::LearnNewShape()
     shapeVector=largeBackground;
     for(int mmr=0; mmr<sizeX+2*boardSize; ++mmr){
       for(int nmr=0; nmr<sizeY+2*boardSize; ++nmr){
-	nx=(mmr-sizeX/2+boardSize)*cos(imr*2*pi/numberOfRotations)+(nmr-sizeY/2+boardSize)*sin(imr*2*pi/numberOfRotations)+sizeX/2;
-	ny=-(mmr-sizeX/2+boardSize)*sin(imr*2*pi/numberOfRotations)+(nmr-sizeY/2+boardSize)*cos(imr*2*pi/numberOfRotations)+sizeY/2;
+	nx=(mmr-sizeX/2-boardSize)*cos(imr*2*pi/numberOfRotations)+(nmr-sizeY/2-boardSize)*sin(imr*2*pi/numberOfRotations)+sizeX/2;
+	ny=-(mmr-sizeX/2-boardSize)*sin(imr*2*pi/numberOfRotations)+(nmr-sizeY/2-boardSize)*cos(imr*2*pi/numberOfRotations)+sizeY/2;
 	if(nx>0 && nx<sizeX && ny>0 && ny<sizeY && croppedCentredBackground[nx][ny]>0.5){
 	  shapeVector[mmr][nmr]=1;
 	}
